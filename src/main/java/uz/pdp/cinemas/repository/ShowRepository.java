91ce6a5cdfd9879d4c1b9b50f6f6a11a70dfc571
ツ <javaguru1716@gmail.com>
2024-03-08 20:47:33 +0500

    @Query for ShowRepository is added

--- src/main/java/uz/pdp/cinemas/repository/ShowRepository.java
+++ src/main/java/uz/pdp/cinemas/repository/ShowRepository.java
@@ -1,6 +1,7 @@
 package uz.pdp.cinemas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import uz.pdp.cinemas.entity.Show;
 
 import java.util.List;
@@ -8,7 +9,9 @@ import java.util.Optional;
 
 public interface ShowRepository extends JpaRepository<Show, Long> {
     Optional<Show> findByName(String name);
+    @Query("SELECT s FROM Show s WHERE s.movie.id = ?1")
     List<Show> findShowsByMovieId(Long id);
+    @Query("SELECT s FROM Show s WHERE s.theater.id = ?1")
     List<Show> findShowsByTheaterId(Long id);
 }
 
